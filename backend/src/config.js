eed3efcb82fa477179eb041223ea43864b89d664
Ignacio Rehbein <78046088+Nach0t@users.noreply.github.com>
2025-07-02 00:30:27 -0400

    Docker

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+// config.js
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env.docker') });
 
 module.exports = {
   mongoURI: process.env.MONGO_URI,
